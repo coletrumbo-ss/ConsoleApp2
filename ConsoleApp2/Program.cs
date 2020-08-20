b8da2d2e0751754e113d4e0ff039db4d767ffbbc
Cole <cole.trumbo@smoothstack.com>
2020-08-20 11:17:44 -0400

    refactor, error-free run on data.csv

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -9,10 +9,11 @@ namespace Dynamics365CRUD
 {
     class Program
     {
-        private const string trialAccount = "https://sscole5.crm.dynamics.com/";
+        private const string trialAccount = "https://orgf3fa0ec3.crm.dynamics.com";
         private const string password = "2nephi3120.D";
-        private const string username = "ct@sscole5.onmicrosoft.com";
+        private const string username = "ct@sscole6.onmicrosoft.com";
         private const string filename = @"data.csv";
+        //private const string filename = @"test-lookups-contact.csv";
         static void Main(string[] args)
         {
             try
@@ -51,94 +52,111 @@ namespace Dynamics365CRUD
                 // Use row to Create, Update, Associate records
                 for (var n = 1; n < num_rows; n++)
                 {
-                    // Read or Create contact
-                    // TODO doctor, minor with parent info
-                    // Doctor has lookup to Account (account tells us doctor is part of private practice, hospital, etc.)
                     Entity contact = new Entity("contact");
                     Entity account = new Entity("account");
                     Entity cole_insuranceplan2 = new Entity("cole_insuranceplan2");
 
                     Guid planId;
+                    Guid contactId;
                     string accountName;
+
+                    // Check if existing contact
                     var firstname = values[n, 0];
                     var lastname = values[n, 1];
                     var email = values[n, 9];
                     var fetchXML = $@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'><entity name='contact'><filter type='and'><condition attribute='emailaddress1' operator='eq' value='{email}' /><condition attribute='firstname' operator='eq' value='{firstname}' /><condition attribute='lastname' operator='eq' value='{lastname}' /></filter></entity></fetch>";
                     var res = conn.GetEntityDataByFetchSearchEC(fetchXML);
-                    if (res.Entities.Count == 0)
+                    contactId = res.Entities.Count > 0 ? res.Entities[0].Id : Guid.Empty;
+                    
+                    contact["cole_birthday3"] = DateTime.Parse(values[n, 2]);
+                    contact["cole_weight"] = decimal.Parse(values[n, 12], CultureInfo.InvariantCulture);
+                    contact["cole_height"] = int.Parse(values[n, 11]);
+                    contact["gendercode"] = new OptionSetValue(int.Parse(values[n, 7]));
+                    contact["firstname"] = values[n, 0];
+                    contact["lastname"] = values[n, 1];
+                    contact["address1_line1"] = values[n, 8];
+                    contact["emailaddress1"] = values[n, 9];
+                    contact["telephone1"] = values[n, 10];
+                    
+                    // Lookup to Insurance Plan (cole_insuranceplan) not required
+                    
+                    // Check for existing Plan
+                    var policynumber = values[n, 5];
+                    var fetchXMLPlan = $@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'><entity name='";
+                    fetchXMLPlan += @"cole_insuranceplan2'><filter type='and'><condition attribute='";
+                    fetchXMLPlan += $@"cole_policynumber' operator='eq' value='{policynumber}' />";
+                    fetchXMLPlan += @"</filter></entity></fetch>";
+                    var resPlan = conn.GetEntityDataByFetchSearchEC(fetchXMLPlan);
+
+                    // Create Insurance Plan (requires Contact ID, Acccount ID)
+                    cole_insuranceplan2["cole_groupnumber"] = int.Parse(values[n, 6]);
+                    cole_insuranceplan2["cole_name"] = values[n, 4];
+                    cole_insuranceplan2["cole_policynumber"] = values[n, 5];
+                    cole_insuranceplan2.Attributes.Add("cole_plantype", new OptionSetValue());
+                    switch (values[n, 4])
+                    {
+                        case "G":
+                            ((OptionSetValue)cole_insuranceplan2["cole_plantype"]).Value = 370510000;
+                            break;
+                        case "S":
+                            ((OptionSetValue)cole_insuranceplan2["cole_plantype"]).Value = 370510001;
+                            break;
+                        case "B":
+                            ((OptionSetValue)cole_insuranceplan2["cole_plantype"]).Value = 370510002;
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+
+                    // Lookup to Account.
+                    // Lookup field is (cole_serviceproviderid) not required, only for doctors (cole_contacttype, cole_licensetype)
+                    // TODO find better way to check existing Account.
+                    // FOR NOW see if account exists. All I have from data.csv is the account name.
+                    // TODO plans are missing Patient, Insurance Company
+                    accountName = values[n, 3];
+                    var fetchXMLAccount = $@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'><entity name='";
+                    fetchXMLAccount += @"account'><filter type='and'><condition attribute='";
+                    fetchXMLAccount += $@"name' operator='eq' value='{accountName}' />";
+                    fetchXMLAccount += @"</filter></entity></fetch>";
+                    var resAccount = conn.GetEntityDataByFetchSearchEC(fetchXMLAccount);
+                    if (resAccount.Entities.Count == 0)
+                    {
+                        // Create Account
+                        account["name"] = values[n, 3];
+                        Guid accountId = service.Create(account);
+                        Console.WriteLine("New account id: {0}. Name: {1}", accountId.ToString(), accountName);
+                        // TODO add all data to account.
+                    }
+
+                    if (resPlan.Entities.Count == 0)
+                    {
+                        planId = service.Create(cole_insuranceplan2);
+                        Console.WriteLine("Plan built under Account: " + accountName);
+                        Console.WriteLine("New cole_insuranceplan2 id: {0}.", planId.ToString());
+                    }
+                    else
+                    {
+                        planId = (Guid)resPlan.Entities[0].Id;
+                        cole_insuranceplan2.Id = planId;
+                        service.Update(cole_insuranceplan2);
+                    }
+                    contact.Attributes.Add("cole_insuranceplan", new EntityReference("cole_insuranceplan2", planId));
+
+                    if (contactId == Guid.Empty)
                     {
                         // Create Contact
-                        // Lookup to Insurance Plan 
-                        var policynumber = values[n, 5];
-                        var fetchXMLPlan = $@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'><entity name='";
-                        fetchXMLPlan += @"cole_insuranceplan2'><filter type='and'><condition attribute='";
-                        fetchXMLPlan += $@"cole_policynumber' operator='eq' value='{policynumber}' />";
-                        fetchXMLPlan += @"</filter></entity></fetch>";
-                        var resPlan = conn.GetEntityDataByFetchSearchEC(fetchXMLPlan);
-                        if (resPlan.Entities.Count == 0)
-                        {
-                            // Create Insurance Plan (requires Contact ID, Acccount ID)
-                            // Lookup to Account
-                            // TODO find better way to check existing Account.
-                            // FOR NOW see if account exists. All I have from data.csv is the account name.
-                            accountName = values[n, 3];
-                            var fetchXMLAccount = $@"<fetch version='1.0' output-format='xml-platform' mapping='logical' distinct='false'><entity name='";
-                            fetchXMLAccount += @"account'><filter type='and'><condition attribute='";
-                            fetchXMLAccount += $@"name' operator='eq' value='{accountName}' />";
-                            fetchXMLAccount += @"</filter></entity></fetch>";
-                            var resAccount = conn.GetEntityDataByFetchSearchEC(fetchXMLAccount);
-                            if (resAccount.Entities.Count == 0)
-                            {
-                                // Create Account
-                                account["name"] = values[n, 3];
-                                Guid accountId = service.Create(account);
-                                Console.WriteLine("New account id: {0}. Name: {1}", accountId.ToString(), accountName);
-                                // TODO add all data to account.
-                            }
-                            cole_insuranceplan2["cole_groupnumber"] = int.Parse(values[n, 6]);
-                            cole_insuranceplan2["cole_name"] = values[n, 4];
-                            cole_insuranceplan2["cole_policynumber"] = values[n, 5];
-                            cole_insuranceplan2.Attributes.Add("cole_plantype", new OptionSetValue());
-                            switch (values[n, 4])
-                            {
-                                case "G":
-                                    ((OptionSetValue)cole_insuranceplan2["cole_plantype"]).Value = 370510000;
-                                    break;
-                                case "S":
-                                    ((OptionSetValue)cole_insuranceplan2["cole_plantype"]).Value = 370510001;
-                                    break;
-                                case "B":
-                                    ((OptionSetValue)cole_insuranceplan2["cole_plantype"]).Value = 370510002;
-                                    break;
-                                default:
-                                    throw new Exception();
-                            }
-                            planId = service.Create(cole_insuranceplan2);
-                            Console.WriteLine("Plan built under Account: " + accountName);
-                            Console.WriteLine("New cole_insuranceplan2 id: {0}.", planId.ToString());
-                        }
-                        else
-                        {
-                            planId = (Guid)resPlan.Entities[0]["Id"];
-                        }
-                        // Create
-                        contact["cole_birthday3"] = DateTime.Parse(values[n, 2]);
-                        contact["cole_weight"] = decimal.Parse(values[n, 12], CultureInfo.InvariantCulture);
-                        contact["cole_height"] = int.Parse(values[n, 11]);
-                        contact["gendercode"] = new OptionSetValue(int.Parse(values[n, 7]));
-                        contact["firstname"] = values[n, 0];
-                        contact["lastname"] = values[n, 1];
-                        contact["address1_line1"] = values[n, 8];
-                        contact["emailaddress1"] = values[n, 9];
-                        contact["telephone1"] = values[n, 10];
-                        // ref t oplan ? contact["cole_serviceproviderid"] = new EntityReference("account", planId)
-
-                        Guid contactId = service.Create(contact);
+                        // TODO doctor, minor with parent info
+                        // Doctor has lookup to Account (account tells us doctor is part of private practice, hospital, etc.)
+                        contactId = service.Create(contact);
                         Console.WriteLine("New contact id: {0}.", contactId.ToString());
 
                         // TODO update insurance plan with patient
                     }
-                    // if you can find the contact try to update it.
+                    else
+                    {
+                        contact.Id = contactId;
+                        service.Update(contact); //Entity Id must be specified for Update
+                    }
                 }
             }
             catch (Exception ex)
